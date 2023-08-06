c4726a2c6c24b45352c730d43374609d97c71d74
seorang42 <seorang42@naver.com>
2023-08-06 22:48:40 +0900

    9.6 Home Screen part Two

--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,25 @@
 const API_KEY = "d9813bd5b92230bf6a02b02506c10734";
 const BASE_PATH = "https://api.themoviedb.org/3";
 
+export interface IMovie {
+  id: number;
+  backdrop_path: string;
+  poster_path: string;
+  title: string;
+  overview: string;
+}
+
+export interface IGetMoviesResult {
+  dates: {
+    maximum: string;
+    minimum: string;
+  };
+  page: number;
+  results: IMovie[];
+  total_pages: number;
+  total_results: number;
+}
+
 export function getMovies() {
   return fetch(
     `${BASE_PATH}//movie/now_playing?api_key=${API_KEY}&language=ko&page=1&region=kr`
